efeb34fb8f40792c2d730ec6c2b119a2d9bccb1a
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-04-08 20:09:29 +0100

    Added Sonification state

--- app/src/main/java/uk/ac/warwick/cim/voiceui/Sonification.java
+++ app/src/main/java/uk/ac/warwick/cim/voiceui/Sonification.java
@@ -2,7 +2,23 @@ package uk.ac.warwick.cim.voiceui;
 
 public class Sonification {
 
-    public enum sonifyState {
+    private boolean rms = false;
 
+    private boolean audio = false;
+
+    public boolean getRmsState () {
+        return rms;
+    }
+
+    public boolean getAudioState () {
+        return audio;
+    }
+
+    public void setRmsState () {
+        rms = (rms)? false: true;
+    }
+
+    public void setAudioState () {
+        audio = (audio)? false: true;
     }
 }
